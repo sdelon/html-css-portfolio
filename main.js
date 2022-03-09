e57ab9d57fa148ecc10f765b729fa39c2c726bb6
sdelon <delon.steph@gmail.com>
2022-03-09 09:05:44 +0100

    3 files modified

--- main.js
+++ main.js
@@ -1,9 +1,1 @@
 console.log('Hello');
-const fruits = ['pommes', 'bananes', 'poires', 'raisins'];
-console.log(fruits)
-const pomme = fruits.filter(fruit => fruit.includes('p'))
-console.log(pomme)
-
-const annee_en_cours = document.querySelector('#year');
-const date = new(Date)
-annee_en_cours.textContent = date.getFullYear();
