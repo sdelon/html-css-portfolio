aa83e75454d1b435346b6fb4d59c2e91645b9424
sdelon <delon.steph@gmail.com>
2022-03-09 10:48:09 +0100

    3 files modified, 6 added

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log('Hello');
+const menu_icon = document.querySelector('#menu-icon');
+const small_menu = document.querySelector('#show-menu');
+
+const open_menu = e => {
+  e.preventDefault();
+  console.log('icon clicked');
+  small_menu.classList.toggle('visible');
+}
+
+menu_icon.addEventListener('click', open_menu);
