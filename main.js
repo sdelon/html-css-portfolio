3542717435746f7ef002dc336b9353c414fe5f57
sdelon <delon.steph@gmail.com>
2022-03-09 13:52:57 +0100

    2 files modified

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Toggle menu icon
 const menu_icon = document.querySelector('#menu-icon');
 const small_menu = document.querySelector('#show-menu');
 
@@ -8,3 +9,15 @@ const open_menu = e => {
 }
 
 menu_icon.addEventListener('click', open_menu);
+
+// Modifier la couleur des steps
+const steps = document.querySelector('.steps-list');
+
+const toggle_step_active = e => {
+  const current_step = e.target; 
+  console.dir(current_step)
+
+  current_step.classList.contains('is-step-active') ? current_step.classList.remove('is-step-active') : current_step.classList.add('is-step-active');
+}
+
+steps.addEventListener('click', toggle_step_active);
