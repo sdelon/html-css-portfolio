24c91dccbfb340ebb318c495f0473e1e4e900f03
sdelon <delon.steph@gmail.com>
2024-03-27 11:57:54 +0100

    1 file modified

--- main.js
+++ main.js
@@ -2,9 +2,7 @@
 const menu_icon = document.querySelector('#menu-icon');
 const small_menu = document.querySelector('#show-menu');
 
-const open_menu = e => {
-  e.preventDefault();
-  console.log('icon clicked');
+const open_menu = () => {
   small_menu.classList.toggle('visible');
 }
 
@@ -13,8 +11,8 @@ menu_icon.addEventListener('click', open_menu);
 // Modifier la couleur des steps
 const steps = document.querySelectorAll('.step-round');
 
-const toggle_step_active = e => {
-  const current_step = e.target.parentNode; 
+const toggle_step_active = event => {
+  const current_step = event.target.parentNode; 
   const all_steps = [...steps].map(step => step.parentNode)
 
   all_steps.filter(step => {
